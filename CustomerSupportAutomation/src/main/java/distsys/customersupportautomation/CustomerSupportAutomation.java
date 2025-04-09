ecff95547bd5040c3a6e2cedc11d8a63add5cd17
Rose <rosipdf@gmail.com>
2025-04-09 11:30:04 +0100

    Proto correction

---
+++ CustomerSupportAutomation/src/main/java/distsys/customersupportautomation/CustomerSupportAutomation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package distsys.customersupportautomation;
+
+/**
+ *
+ * @author macbook
+ */
+public class CustomerSupportAutomation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
